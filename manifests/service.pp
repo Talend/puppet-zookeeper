b5346b8c59f83af165bf4158c1757ea55f93e9ac
aheumaier <andreas.heumaier@nordcloud.com>
2016-06-13 11:33:30 +0200

    Set exhibitor tomcat service right

--- manifests/service.pp
+++ manifests/service.pp
@@ -2,6 +2,7 @@ class zookeeper::service (
 
   $service_ensure = $zookeeper::service_ensure,
   $service_enable = $zookeeper::service_eable,
+  $catalina_base  = $zookeeper::exhibitor_catalina_base,
 
 ){
 
@@ -9,6 +10,11 @@ class zookeeper::service (
   service{'zookeeper':
     ensure => $service_ensure,
     enable => $service_enable,
+  }
+
+  tomcat::service { 'syncope':
+    catalina_base => $catalina_base,
+    use_init      => false,
   }
 
 
