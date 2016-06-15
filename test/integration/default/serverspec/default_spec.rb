171e210c72874bc096f7c3a2fadbf19d68a36ffc
Alexander Grigoriev <agrigoriev@talend.com>
2016-06-15 14:19:50 +0200

    added simple test spec

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,4 +1,7 @@
 require 'spec_helper'
 
 describe 'zookeeper' do
+  describe command('/usr/bin/wget -O - http://127.0.0.1:8080/exhibitor/exhibitor/v1/cluster/state') do
+    its(:stdout) { should include '"description":"serving"' }
+  end
 end
