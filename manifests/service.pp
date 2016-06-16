61c00922e82e7f82e3774d278f1e06178b9d01ba
Alexander Grigoriev <agrigoriev@talend.com>
2016-06-16 08:56:13 +0200

    removed zookeeper service initialization

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,15 +1,8 @@
 class zookeeper::service (
 
-  $service_ensure = $zookeeper::service_ensure,
-  $service_enable = $zookeeper::service_eable,
   $catalina_base  = $zookeeper::exhibitor_catalina_base,
 
 ){
-
-  service{ 'zookeeper':
-    ensure => $service_ensure,
-    enable => $service_enable,
-  }
 
   tomcat::service { 'exhibitor':
     catalina_base => $catalina_base,
