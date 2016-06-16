8d88781697653be7af6f4027c3f28e9c918a6817
Alexander Grigoriev <agrigoriev@talend.com>
2016-06-16 08:55:38 +0200

    two obsolete parameters removed

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,8 +1,6 @@
 class zookeeper::params {
 
   $package_ensure = 'installed'
-  $service_ensure = 'running'
-  $service_enable = 'enabled'
   $tomcat_version = '7'
   $exhibitor_catalina_base = '/opt/apache-tomcat'
 
