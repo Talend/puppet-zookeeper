d2b9ce89cb8140f38a46ffc7df5d86cd20d3f0ab
Alexander Grigoriev <agrigoriev@talend.com>
2016-08-23 09:45:38 +0200

    removing parameters from the service class

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,14 +1,13 @@
-class zookeeper::service (
+class zookeeper::service {
 
-  $catalina_base  = $zookeeper::exhibitor_catalina_base,
-
-){
+  require ::zookeeper::config
 
   ensure_packages(['wget'])
 
   tomcat::service { 'exhibitor':
-    catalina_base => $catalina_base,
-    use_init      => false,
+    service_ensure => $zookeeper::service_ensure,
+    catalina_base  => $zookeeper::catalina_base,
+    use_init       => false,
   } ->
   exec { 'waiting for exhibitor to start':
     command => '/usr/bin/wget --spider --tries 15 --retry-connrefused http://localhost:8080/exhibitor/exhibitor/v1/config/get-state',
@@ -16,9 +15,6 @@ class zookeeper::service (
   } ->
   exec { 'update the config':
     command => '/usr/bin/curl -X POST -d \'{"logIndexDirectory":"","zookeeperInstallDirectory":"/usr/lib/zookeeper","zookeeperDataDirectory":"/var/lib/zookeeper/data","zookeeperLogDirectory":"","serversSpec":"","backupExtra":"","zooCfgExtra":{"syncLimit":"5","tickTime":"2000","initLimit":"10"},"javaEnvironment":"","log4jProperties":"","clientPort":2181,"connectPort":2888,"electionPort":3888,"checkMs":30000,"cleanupPeriodMs":43200000,"cleanupMaxFiles":3,"backupMaxStoreMs":86400000,"backupPeriodMs":60000,"autoManageInstances":0,"autoManageInstancesSettlingPeriodMs":180000,"observerThreshold":999,"autoManageInstancesFixedEnsembleSize":0,"autoManageInstancesApplyAllAtOnce":1,"controlPanel":{}}\' http://localhost:8080/exhibitor/exhibitor/v1/config/set'
-  } ->
-  exec { 'wait till exhibitor starts zookeeper':
-    command => '/usr/bin/sleep 30'
   }
 
 }
