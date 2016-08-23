7383586ba0d3d8458797f62262b3a63ffc48d6b3
Alexander Grigoriev <agrigoriev@talend.com>
2016-08-23 10:06:46 +0200

    sorted_json function added

--- manifests/service.pp
+++ manifests/service.pp
@@ -4,17 +4,49 @@ class zookeeper::service {
 
   ensure_packages(['wget'])
 
+  $config = {
+    "logIndexDirectory"         => "",
+    "zookeeperInstallDirectory" => "/usr/lib/zookeeper",
+    "zookeeperDataDirectory"    => "/var/lib/zookeeper/data",
+    "zookeeperLogDirectory"     => "",
+    "serversSpec"               => "",
+    "backupExtra"               => "",
+    "zooCfgExtra"               => {
+        "syncLimit" => "5",
+        "tickTime"  => "2000",
+        "initLimit" => "10",
+    },
+    "javaEnvironment"                      => "",
+    "log4jProperties"                      => "",
+    "clientPort"                           => 2181,
+    "connectPort"                          => 2888,
+    "electionPort"                         => 3888,
+    "checkMs"                              => 30000,
+    "cleanupPeriodMs"                      => 43200000,
+    "cleanupMaxFiles"                      => 3,
+    "backupMaxStoreMs"                     => 86400000,
+    "backupPeriodMs"                       => 60000,
+    "autoManageInstances"                  => 0,
+    "autoManageInstancesSettlingPeriodMs"  => 180000,
+    "observerThreshold"                    => 999,
+    "autoManageInstancesFixedEnsembleSize" => 0,
+    "autoManageInstancesApplyAllAtOnce"    => 1,
+    "controlPanel"                         => {},
+  }
+  $config_json    = sorted_json($config)
+  $exhibitor_port = $zookeeper::exhibitor_port
+
   tomcat::service { 'exhibitor':
     service_ensure => $zookeeper::service_ensure,
     catalina_base  => $zookeeper::catalina_base,
     use_init       => false,
   } ->
   exec { 'waiting for exhibitor to start':
-    command => '/usr/bin/wget --spider --tries 15 --retry-connrefused http://localhost:8080/exhibitor/exhibitor/v1/config/get-state',
+    command => "/usr/bin/wget --spider --tries 15 --retry-connrefused http://localhost:${exhibitor_port}/exhibitor/exhibitor/v1/config/get-state",
     require => Package['wget']
   } ->
   exec { 'update the config':
-    command => '/usr/bin/curl -X POST -d \'{"logIndexDirectory":"","zookeeperInstallDirectory":"/usr/lib/zookeeper","zookeeperDataDirectory":"/var/lib/zookeeper/data","zookeeperLogDirectory":"","serversSpec":"","backupExtra":"","zooCfgExtra":{"syncLimit":"5","tickTime":"2000","initLimit":"10"},"javaEnvironment":"","log4jProperties":"","clientPort":2181,"connectPort":2888,"electionPort":3888,"checkMs":30000,"cleanupPeriodMs":43200000,"cleanupMaxFiles":3,"backupMaxStoreMs":86400000,"backupPeriodMs":60000,"autoManageInstances":0,"autoManageInstancesSettlingPeriodMs":180000,"observerThreshold":999,"autoManageInstancesFixedEnsembleSize":0,"autoManageInstancesApplyAllAtOnce":1,"controlPanel":{}}\' http://localhost:8080/exhibitor/exhibitor/v1/config/set'
+    command => "/usr/bin/curl -X POST -d '${config_json}' http://localhost:${exhibitor_port}/exhibitor/exhibitor/v1/config/set"
   }
 
 }
