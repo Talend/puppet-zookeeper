dba2f208240129c610b4189fd13c9e7ace57f69c
Alexander Grigoriev <agrigoriev@talend.com>
2016-08-23 14:21:42 +0200

    changing the way how the exhibitor is installed

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,12 +1,5 @@
 class zookeeper::install {
 
-  unless defined(File['/opt/tomcat']){
-    file{ '/opt/tomcat':
-      ensure => 'link',
-      target => $zookeeper::catalina_base
-    }
-  }
-
   tomcat::instance { 'ROOT':
     install_from_source => true,
     source_url          => $zookeeper::tomcat_source_url,
@@ -16,8 +9,22 @@ class zookeeper::install {
     'zookeeper':
       ensure => installed;
     'netflix-exhibitor-tomcat':
-      ensure  => installed,
-      require => File['/opt/tomcat'];
+      ensure  => installed;
+  } ->
+  exec { 'publishing exhibitor : 1':
+    cwd     => $zookeeper::catalina_base,
+    command => "/usr/bin/rm -rf webapps/docs webapps/examples webapps/host-manager webapps/manager webapps/ROOT && /usr/bin/touch /tmp/exhibitor.1",
+    creates => '/tmp/exhibitor.1'
+  } ->
+  exec { 'publishing exhibitor : 2':
+    cwd     => $zookeeper::catalina_base,
+    command => "/usr/bin/ln -s /opt/tomcat/webapps/exhibitor /opt/apache-tomcat/webapps/ROOT && /usr/bin/touch /tmp/exhibitor.2",
+    creates => '/tmp/exhibitor.2'
+  } ->
+  exec { 'publishing exhibitor : 3':
+    cwd     => $zookeeper::catalina_base,
+    command => "/usr/bin/chown -R tomcat:tomcat /opt/tomcat && /usr/bin/touch /tmp/exhibitor.3",
+    creates => '/tmp/exhibitor.3'
   }
 
 }
