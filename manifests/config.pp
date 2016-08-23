2721ad1dfd36bcb25a683460cbf6f0981162fa90
Alexander Grigoriev <agrigoriev@talend.com>
2016-08-23 09:29:31 +0200

    removed parameters from the config class

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,13 +1,6 @@
-class zookeeper::config (
+class zookeeper::config {
 
-  $exhibitor_catalina_base = $zookeeper::exhibitor_catalina_base,
-  $jmx_enabled             = false,
-  $backup_enabled          = false
-
-) {
-
-  validate_bool($jmx_enabled)
-  validate_bool($backup_enabled)
+  require ::zookeeper::install
 
   #This directories have to be accessible by tomcat
   $zookeeper_dirs = [
@@ -19,10 +12,10 @@ class zookeeper::config (
   ]
 
   file {
-    "${exhibitor_catalina_base}/webapps/exhibitor/WEB-INF/classes/exhibitor.properties":
-      content => template('zookeeper/exhibitor.properties.erb'),
-      owner   => 'tomcat',
-      group   => 'tomcat';
+   "${zookeeper::catalina_base}/webapps/exhibitor/WEB-INF/classes/exhibitor.properties":
+     content => template('zookeeper/exhibitor.properties.erb'),
+     owner   => 'tomcat',
+     group   => 'tomcat';
     $zookeeper_dirs:
       ensure => directory,
       owner  => 'tomcat',
