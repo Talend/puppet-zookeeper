8aed4403a65128f32f3a4608a9a36761296b7067
Alexander Grigoriev <agrigoriev@talend.com>
2016-08-23 14:35:18 +0200

    adjusting specs

--- spec/acceptance/zookeeper_spec.rb
+++ spec/acceptance/zookeeper_spec.rb
@@ -2,18 +2,21 @@ require 'spec_helper'
 
 describe 'zookeeper' do
 
-  context 'When Zookeeper installed' do
+  context 'When Exhibitor installed' do
     describe 'Tomcat Server Info' do
       subject { command('/usr/bin/java -cp /opt/apache-tomcat/lib/catalina.jar org.apache.catalina.util.ServerInfo') }
       its(:stdout) { should include 'Apache Tomcat/8.5.2' }
     end
   end
 
-  describe port(2181) do
-    it { should be_listening }
+  context 'When Exhibitor running' do
+    describe port(8080) do
+      it { should be_listening }
+    end
+
+    describe command('/usr/bin/wget -O - http://127.0.0.1:8080/exhibitor/v1/cluster/state') do
+      its(:stdout) { should match /"description":"(serving|latent)"/ }
+    end
   end
 
-  describe command('/usr/bin/wget -O - http://127.0.0.1:8080/exhibitor/v1/cluster/state') do
-    its(:stdout) { should include '"description":"serving"' }
-  end
 end
