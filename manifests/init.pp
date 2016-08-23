e41f993e721b6819a27d4add240ae4fc345740aa
Alexander Grigoriev <agrigoriev@talend.com>
2016-08-23 10:57:21 +0200

    added backup-related parameters to the main class

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,13 @@
 class zookeeper (
 
-  $tomcat_source_url  = 'http://archive.apache.org/dist/tomcat/tomcat-8/v8.5.2/bin/apache-tomcat-8.5.2.tar.gz',
-  $catalina_base      = '/opt/apache-tomcat',
-  $service_ensure     = running,
-  $exhibitor_port     = 8080,
-  $zookeeper_nodes    = [],
+  $tomcat_source_url    = 'http://archive.apache.org/dist/tomcat/tomcat-8/v8.5.2/bin/apache-tomcat-8.5.2.tar.gz',
+  $catalina_base        = '/opt/apache-tomcat',
+  $service_ensure       = running,
+  $exhibitor_port       = 8080,
+  $backup_enable        = false,
+  $backup_bucket_name   = undef,
+  $backup_bucket_prefix = undef,
+  $zookeeper_nodes      = [],
 
 ) {
 
