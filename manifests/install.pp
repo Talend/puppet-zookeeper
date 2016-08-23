40c8ea9034c5060bb0816ecda35aa4917241a112
Alexander Grigoriev <agrigoriev@talend.com>
2016-08-23 09:32:29 +0200

    removed parameters from the install class

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,35 +1,24 @@
-class zookeeper::install (
-
-  $package_ensure          = $zookeeper::package_ensure,
-  $exhibitor_catalina_base = $zookeeper::exhibitor_catalina_base,
-  $tomcat_version          = $zookeeper::tomcat_version,
-
-){
-
-  $source_url = $tomcat_version ? {
-    '7'     => 'http://archive.apache.org/dist/tomcat/tomcat-7/v7.0.69/bin/apache-tomcat-7.0.69.tar.gz',
-    default => 'http://archive.apache.org/dist/tomcat/tomcat-8/v8.5.2/bin/apache-tomcat-8.5.2.tar.gz'
-  }
+class zookeeper::install {
 
   unless defined(File['/opt/tomcat']){
     file{ '/opt/tomcat':
       ensure => 'link',
-      target => $exhibitor_catalina_base
+      target => $zookeeper::catalina_base
     }
   }
 
   tomcat::instance { 'ROOT':
     install_from_source => true,
-    source_url          => $source_url,
-    catalina_base       => $exhibitor_catalina_base,
+    source_url          => $zookeeper::tomcat_source_url,
+    catalina_base       => $zookeeper::catalina_base,
     java_home           => $::java_default_home,
   } ->
-
   package {
     'zookeeper':
-      ensure => $package_ensure;
+      ensure => $zookeeper::service_ensure;
     'netflix-exhibitor-tomcat':
-      ensure  => $package_ensure,
+      ensure  => $zookeeper::service_ensure,
       require => File['/opt/tomcat'];
   }
+
 }
