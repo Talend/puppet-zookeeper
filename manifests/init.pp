ba58ebb2510ab741afc29be5aba5e403e1b28927
Alexander Grigoriev <agrigoriev@talend.com>
2017-02-27 17:19:23 +0100

    adding service_enable parameter

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,7 @@ class zookeeper (
   $tomcat_source_url    = 'http://archive.apache.org/dist/tomcat/tomcat-8/v8.5.2/bin/apache-tomcat-8.5.2.tar.gz',
   $catalina_base        = '/opt/apache-tomcat',
   $service_ensure       = running,
+  $service_enable       = true,
   $exhibitor_port       = 8080,
   $backup_enable        = false,
   $backup_bucket_name   = undef,
